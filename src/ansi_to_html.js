dae1b77549ce7d68cdf46a223f08533577a32808
Rob Burns <rburns@paiges.net>
2017-02-17 08:09:21 +0700

    satisfy linter

--- src/ansi_to_html.js
+++ src/ansi_to_html.js
@@ -1,47 +1,47 @@
-const entities = require('entities'),
-  defaults = {
+const entities = require('entities');
+const defaults = {
     fg: '#FFF',
     bg: '#000',
     newline: false,
     escapeXML: false,
     stream: false,
     colors: getDefaultColors()
-  };
+};
 
 function getDefaultColors() {
-  const colors = {
-    0: '#000',
-    1: '#A00',
-    2: '#0A0',
-    3: '#A50',
-    4: '#00A',
-    5: '#A0A',
-    6: '#0AA',
-    7: '#AAA',
-    8: '#555',
-    9: '#F55',
-    10: '#5F5',
-    11: '#FF5',
-    12: '#55F',
-    13: '#F5F',
-    14: '#5FF',
-    15: '#FFF'
-  };
+    const colors = {
+        0: '#000',
+        1: '#A00',
+        2: '#0A0',
+        3: '#A50',
+        4: '#00A',
+        5: '#A0A',
+        6: '#0AA',
+        7: '#AAA',
+        8: '#555',
+        9: '#F55',
+        10: '#5F5',
+        11: '#FF5',
+        12: '#55F',
+        13: '#F5F',
+        14: '#5FF',
+        15: '#FFF'
+    };
 
-  range(0, 5).forEach(red => {
-    range(0, 5).forEach(green => {
-      range(0, 5).forEach(blue => setStyleColor(red, green, blue, colors));
+    range(0, 5).forEach(red => {
+        range(0, 5).forEach(green => {
+            range(0, 5).forEach(blue => setStyleColor(red, green, blue, colors));
+        });
     });
-  });
 
-  range(0, 23).forEach(function (gray) {
-    const c = gray + 232,
-      l = toHexString(gray * 10 + 8);
+    range(0, 23).forEach(function (gray) {
+        const c = gray + 232;
+        const l = toHexString(gray * 10 + 8);
 
-    colors[c] = '#' + l + l + l;
-  });
+        colors[c] = '#' + l + l + l;
+    });
 
-  return colors;
+    return colors;
 }
 
 /**
@@ -51,12 +51,12 @@ function getDefaultColors() {
  * @param {object} colors
  */
 function setStyleColor(red, green, blue, colors) {
-  const c = 16 + (red * 36) + (green * 6) + blue,
-    r = red > 0 ? red * 40 + 55 : 0,
-    g = green > 0 ? green * 40 + 55 : 0,
-    b = blue > 0 ? blue * 40 + 55 : 0;
+    const c = 16 + (red * 36) + (green * 6) + blue;
+    const r = red > 0 ? red * 40 + 55 : 0;
+    const g = green > 0 ? green * 40 + 55 : 0;
+    const b = blue > 0 ? blue * 40 + 55 : 0;
 
-  colors[c] = toColorHexString([r, g, b]);
+    colors[c] = toColorHexString([r, g, b]);
 }
 
 /**
@@ -65,13 +65,13 @@ function setStyleColor(red, green, blue, colors) {
  * @returns {string}
  */
 function toHexString(num) {
-  var str = num.toString(16);
+    var str = num.toString(16);
 
-  while (str.length < 2) {
-    str = '0' + str;
-  }
+    while (str.length < 2) {
+        str = '0' + str;
+    }
 
-  return str;
+    return str;
 }
 
 /**
@@ -80,13 +80,13 @@ function toHexString(num) {
  * @returns {string}
  */
 function toColorHexString(ref) {
-  var results = [];
+    var results = [];
 
-  for (var j = 0, len = ref.length; j < len; j++) {
-    results.push(toHexString(ref[j]));
-  }
+    for (var j = 0, len = ref.length; j < len; j++) {
+        results.push(toHexString(ref[j]));
+    }
 
-  return '#' + results.join('');
+    return '#' + results.join('');
 }
 
 /**
@@ -96,17 +96,17 @@ function toColorHexString(ref) {
  * @param {object} options
  */
 function generateOutput(stack, token, data, options) {
-  var result;
+    var result;
 
-  if (token === 'text') {
-    result = pushText(data, options);
-  } else if (token === 'display') {
-    result = handleDisplay(stack, data, options);
-  } else if (token === 'xterm256') {
-    result = pushForegroundColor(stack, options.colors[data]);
-  }
+    if (token === 'text') {
+        result = pushText(data, options);
+    } else if (token === 'display') {
+        result = handleDisplay(stack, data, options);
+    } else if (token === 'xterm256') {
+        result = pushForegroundColor(stack, options.colors[data]);
+    }
 
-  return result;
+    return result;
 }
 
 /**
@@ -116,39 +116,39 @@ function generateOutput(stack, token, data, options) {
  * @returns {*}
  */
 function handleDisplay(stack, code, options) {
-  code = parseInt(code, 10);
-  var result;
+    code = parseInt(code, 10);
+    var result;
 
-  const codeMap = {
-    '-1': () => '<br/>',
-    0: () => stack.length && resetStyles(stack),
-    1: () => pushTag(stack, 'b'),
-    3: () => pushTag(stack, 'i'),
-    4: () => pushTag(stack, 'u'),
-    8: () => pushStyle(stack, 'display:none'),
-    9: () => pushTag(stack, 'strike'),
-    22: () => closeTag(stack, 'b'),
-    23: () => closeTag(stack, 'i'),
-    24: () => closeTag(stack, 'u'),
-    39: () => pushForegroundColor(stack, options.fg),
-    49: () => pushBackgroundColor(stack, options.bg)
-  };
+    const codeMap = {
+        '-1': () => '<br/>',
+        0: () => stack.length && resetStyles(stack),
+        1: () => pushTag(stack, 'b'),
+        3: () => pushTag(stack, 'i'),
+        4: () => pushTag(stack, 'u'),
+        8: () => pushStyle(stack, 'display:none'),
+        9: () => pushTag(stack, 'strike'),
+        22: () => closeTag(stack, 'b'),
+        23: () => closeTag(stack, 'i'),
+        24: () => closeTag(stack, 'u'),
+        39: () => pushForegroundColor(stack, options.fg),
+        49: () => pushBackgroundColor(stack, options.bg)
+    };
 
-  if (codeMap[code]) {
-    result = codeMap[code]();
-  } else if (4 < code && code < 7) {
-    result = pushTag(stack, 'blink');
-  } else if (29 < code && code < 38) {
-    result = pushForegroundColor(stack, options.colors[code - 30]);
-  } else if ((39 < code && code < 48)) {
-    result = pushBackgroundColor(stack, options.colors[code - 40]);
-  } else if ((89 < code && code < 98)) {
-    result = pushForegroundColor(stack, options.colors[8 + (code - 90)]);
-  } else if ((99 < code && code < 108)) {
-    result = pushBackgroundColor(stack, options.colors[8 + (code - 100)]);
-  }
+    if (codeMap[code]) {
+        result = codeMap[code]();
+    } else if (4 < code && code < 7) {
+        result = pushTag(stack, 'blink');
+    } else if (29 < code && code < 38) {
+        result = pushForegroundColor(stack, options.colors[code - 30]);
+    } else if ((39 < code && code < 48)) {
+        result = pushBackgroundColor(stack, options.colors[code - 40]);
+    } else if ((89 < code && code < 98)) {
+        result = pushForegroundColor(stack, options.colors[8 + (code - 90)]);
+    } else if ((99 < code && code < 108)) {
+        result = pushBackgroundColor(stack, options.colors[8 + (code - 100)]);
+    }
 
-  return result;
+    return result;
 }
 
 /**
@@ -156,13 +156,13 @@ function handleDisplay(stack, code, options) {
  * @returns {string}
  */
 function resetStyles(stack) {
-  var stackClone = stack.slice(0);
+    var stackClone = stack.slice(0);
 
-  stack.length = 0;
+    stack.length = 0;
 
-  return stackClone.reverse().map(function (tag) {
-    return '</' + tag + '>';
-  }).join('');
+    return stackClone.reverse().map(function (tag) {
+        return '</' + tag + '>';
+    }).join('');
 }
 
 /**
@@ -173,13 +173,13 @@ function resetStyles(stack) {
  * @example range(3, 7); // creates [3, 4, 5, 6, 7]
  */
 function range(low, high) {
-  const results = [];
+    const results = [];
 
-  for (var j = low; j <= high; j++) {
-    results.push(j);
-  }
+    for (var j = low; j <= high; j++) {
+        results.push(j);
+    }
 
-  return results;
+    return results;
 }
 
 
@@ -190,9 +190,9 @@ function range(low, high) {
  * @returns {function}
  */
 function notCategory(category) {
-  return function (e) {
-    return (category === null || e.category !== category) && category !== 'all';
-  };
+    return function (e) {
+        return (category === null || e.category !== category) && category !== 'all';
+    };
 }
 
 /**
@@ -201,28 +201,28 @@ function notCategory(category) {
  * @returns {string}
  */
 function categoryForCode(code) {
-  code = parseInt(code, 10);
-  var result = null;
+    code = parseInt(code, 10);
+    var result = null;
 
-  if (code === 0) {
-    result = 'all';
-  } else if (code === 1) {
-    result = 'bold';
-  } else if ((2 < code && code < 5)) {
-    result = 'underline';
-  } else if ((4 < code && code < 7)) {
-    result = 'blink';
-  } else if (code === 8) {
-    result = 'hide';
-  } else if (code === 9) {
-    result = 'strike';
-  } else if ((29 < code && code < 38) || code === 39 || (89 < code && code < 98)) {
-    result = 'foreground-color';
-  } else if ((39 < code && code < 48) || code === 49 || (99 < code && code < 108)) {
-    result = 'background-color';
-  }
+    if (code === 0) {
+        result = 'all';
+    } else if (code === 1) {
+        result = 'bold';
+    } else if ((2 < code && code < 5)) {
+        result = 'underline';
+    } else if ((4 < code && code < 7)) {
+        result = 'blink';
+    } else if (code === 8) {
+        result = 'hide';
+    } else if (code === 9) {
+        result = 'strike';
+    } else if ((29 < code && code < 38) || code === 39 || (89 < code && code < 98)) {
+        result = 'foreground-color';
+    } else if ((39 < code && code < 48) || code === 49 || (99 < code && code < 108)) {
+        result = 'background-color';
+    }
 
-  return result;
+    return result;
 }
 
 /**
@@ -231,11 +231,11 @@ function categoryForCode(code) {
  * @returns {string}
  */
 function pushText(text, options) {
-  if (options.escapeXML) {
-    return entities.encodeXML(text);
-  }
+    if (options.escapeXML) {
+        return entities.encodeXML(text);
+    }
 
-  return text;
+    return text;
 }
 
 /**
@@ -245,13 +245,13 @@ function pushText(text, options) {
  * @returns {string}
  */
 function pushTag(stack, tag, style) {
-  if (!style) {
-    style = '';
-  }
+    if (!style) {
+        style = '';
+    }
 
-  stack.push(tag);
+    stack.push(tag);
 
-  return ['<' + tag, (style ? ' style="' + style + '"' : void 0), '>'].join('');
+    return ['<' + tag, (style ? ' style="' + style + '"' : void 0), '>'].join('');
 }
 
 /**
@@ -260,15 +260,15 @@ function pushTag(stack, tag, style) {
  * @returns {string}
  */
 function pushStyle (stack, style) {
-  return pushTag(stack, 'span', style);
+    return pushTag(stack, 'span', style);
 }
 
 function pushForegroundColor(stack, color) {
-  return pushTag(stack, 'span', 'color:' + color);
+    return pushTag(stack, 'span', 'color:' + color);
 }
 
 function pushBackgroundColor(stack, color) {
-  return pushTag(stack, 'span', 'background-color:' + color);
+    return pushTag(stack, 'span', 'background-color:' + color);
 }
 
 /**
@@ -277,15 +277,15 @@ function pushBackgroundColor(stack, color) {
  * @returns {string}
  */
 function closeTag(stack, style) {
-  var last;
+    var last;
 
-  if (stack.slice(-1)[0] === style) {
-    last = stack.pop();
-  }
+    if (stack.slice(-1)[0] === style) {
+        last = stack.pop();
+    }
 
-  if (last) {
-    return '</' + style + '>';
-  }
+    if (last) {
+        return '</' + style + '>';
+    }
 }
 
 /**
@@ -295,105 +295,103 @@ function closeTag(stack, style) {
  * @returns {Array}
  */
 function tokenize(text, options, callback) {
-  var ansiMatch = false,
-    ansiHandler = 3;
+    var ansiMatch = false;
+    var ansiHandler = 3;
 
-  function remove() {
-    return '';
-  }
-
-  function removeXterm256(m, g1) {
-    callback('xterm256', g1);
-    return '';
-  }
-
-  function newline(m) {
-    if (options.newline) {
-      callback('display', -1);
-    } else {
-      callback('text', m);
+    function remove() {
+        return '';
     }
 
-    return '';
-  }
-
-  function ansiMess(m, g1) {
-    ansiMatch = true;
-    if (g1.trim().length === 0) {
-      g1 = '0';
+    function removeXterm256(m, g1) {
+        callback('xterm256', g1);
+        return '';
     }
 
-    g1 = g1.trimRight(';').split(';');
+    function newline(m) {
+        if (options.newline) {
+            callback('display', -1);
+        } else {
+            callback('text', m);
+        }
 
-    for (var o = 0, len = g1.length; o < len; o++) {
-      callback('display', g1[o]);
+        return '';
     }
 
-    return '';
-  }
+    function ansiMess(m, g1) {
+        ansiMatch = true;
+        if (g1.trim().length === 0) {
+            g1 = '0';
+        }
 
-  function realText(m) {
-    callback('text', m);
+        g1 = g1.trimRight(';').split(';');
 
-    return '';
-  }
+        for (var o = 0, len = g1.length; o < len; o++) {
+            callback('display', g1[o]);
+        }
 
-  /* eslint no-control-regex:0 */
-  const tokens = [
-    {
-      pattern: /^\x08+/,
-      sub: remove
+        return '';
+    }
+
+    function realText(m) {
+        callback('text', m);
+
+        return '';
+    }
+
+    /* eslint no-control-regex:0 */
+    const tokens = [{
+        pattern: /^\x08+/,
+        sub: remove
     }, {
-      pattern: /^\x1b\[[012]?K/,
-      sub: remove
+        pattern: /^\x1b\[[012]?K/,
+        sub: remove
     }, {
-      pattern: /^\x1b\[38;5;(\d+)m/,
-      sub: removeXterm256
+        pattern: /^\x1b\[38;5;(\d+)m/,
+        sub: removeXterm256
     }, {
-      pattern: /^\n/,
-      sub: newline
+        pattern: /^\n/,
+        sub: newline
     }, {
-      pattern: /^\x1b\[((?:\d{1,3};?)+|)m/,
-      sub: ansiMess
+        pattern: /^\x1b\[((?:\d{1,3};?)+|)m/,
+        sub: ansiMess
     }, {
-      pattern: /^\x1b\[?[\d;]{0,3}/,
-      sub: remove
+        pattern: /^\x1b\[?[\d;]{0,3}/,
+        sub: remove
     }, {
-      pattern: /^([^\x1b\x08\n]+)/,
-      sub: realText
-    }
-  ];
+        pattern: /^([^\x1b\x08\n]+)/,
+        sub: realText
+    }];
 
-  function process(handler, i) {
-    if (i > ansiHandler && ansiMatch) {
-      return;
-    }
+    function process(handler, i) {
+        if (i > ansiHandler && ansiMatch) {
+            return;
+        }
 
-    ansiMatch = false;
+        ansiMatch = false;
 
-    text = text.replace(handler.pattern, handler.sub);
-  }
-
-  var handler,
-    results1 = [],
-    length = text.length;
-
-  while (length > 0) {
-    for (var i = 0, o = 0, len = tokens.length; o < len; i = ++o) {
-      handler = tokens[i];
-      process(handler, i);
+        text = text.replace(handler.pattern, handler.sub);
     }
 
-    if (text.length === length) {
-      break;
-    } else {
-      results1.push(0);
+    var handler;
+    var results1 = [];
+    var length = text.length;
+
+    while (length > 0) {
+        for (var i = 0, o = 0, len = tokens.length; o < len; i = ++o) {
+            handler = tokens[i];
+            process(handler, i);
+        }
+
+        if (text.length === length) {
+            break;
+        } else {
+            results1.push(0);
+        }
+
+        length = text.length;
     }
 
-    length = text.length;
-  }
-
-  return results1;
+    return results1;
 }
 
 /**
@@ -405,59 +403,59 @@ function tokenize(text, options, callback) {
  * @returns {Array}
  */
 function updateStickyStack(stickyStack, token, data) {
-  if (token !== 'text') {
-    stickyStack = stickyStack.filter(notCategory(categoryForCode(data)));
-    stickyStack.push({token: token, data: data, category: categoryForCode(data)});
-  }
+    if (token !== 'text') {
+        stickyStack = stickyStack.filter(notCategory(categoryForCode(data)));
+        stickyStack.push({token: token, data: data, category: categoryForCode(data)});
+    }
 
-  return stickyStack;
+    return stickyStack;
 }
 
 function Filter(options) {
-  options = options || {};
+    options = options || {};
 
-  if (options.colors) {
-    options.colors = Object.assign({}, defaults.colors, options.colors);
-  }
+    if (options.colors) {
+        options.colors = Object.assign({}, defaults.colors, options.colors);
+    }
 
-  this.opts = Object.assign({}, defaults, options);
-  this.stack = [];
-  this.stickyStack = [];
+    this.opts = Object.assign({}, defaults, options);
+    this.stack = [];
+    this.stickyStack = [];
 }
 
 Filter.prototype = {
-  toHtml (input) {
-    input = typeof input === 'string' ? [input] : input;
-    const stack = this.stack,
-      options = this.opts,
-      buf = [];
+    toHtml (input) {
+        input = typeof input === 'string' ? [input] : input;
+        const stack = this.stack;
+        const options = this.opts;
+        const buf = [];
 
-    this.stickyStack.forEach(element => {
-      var output = generateOutput(stack, element.token, element.data, options);
+        this.stickyStack.forEach(element => {
+            var output = generateOutput(stack, element.token, element.data, options);
 
-      if (output) {
-        buf.push(output);
-      }
-    });
+            if (output) {
+                buf.push(output);
+            }
+        });
 
-    tokenize(input.join(''), options, (token, data) => {
-      var output = generateOutput(stack, token, data, options);
+        tokenize(input.join(''), options, (token, data) => {
+            var output = generateOutput(stack, token, data, options);
 
-      if (output) {
-        buf.push(output);
-      }
+            if (output) {
+                buf.push(output);
+            }
 
-      if (options.stream) {
-        this.stickyStack = updateStickyStack(this.stickyStack, token, data);
-      }
-    });
+            if (options.stream) {
+                this.stickyStack = updateStickyStack(this.stickyStack, token, data);
+            }
+        });
 
-    if (stack.length) {
-      buf.push(resetStyles(stack));
+        if (stack.length) {
+            buf.push(resetStyles(stack));
+        }
+
+        return buf.join('');
     }
-
-    return buf.join('');
-  }
 };
 
 module.exports = Filter;
